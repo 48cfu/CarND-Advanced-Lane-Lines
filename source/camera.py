a8344311b69e003700dbcf9bff4060fcc795155c
48cfu <56238276+48cfu@users.noreply.github.com>
2019-12-26 01:52:15 +0100

    Camera calibration, gradient and color, top view Working versions of camera calibration, gradient and color-treshholds and perspective transformation (birds-eye view)

--- source/camera.py
+++ source/camera.py
@@ -13,7 +13,7 @@ class Camera():
         self.imgpoints = [] # 2d points in image plane.
         
         
-    def calibrate(self, path = '../data/camera_cal/calibration*.jpg'):
+    def calibrate(self, path = '../data/camera_cal/calibration*.jpg', plot_figures = False):
         # Make a list of calibration images
         images = glob.glob(path)
         
@@ -23,13 +23,21 @@ class Camera():
 
         # Step through the list and search for chessboard corners
         i = 0
+        if plot_figures == True:
+            fig = plt.figure()
+        row = 5
+        column = 4
+        gray = None
         for fname in images:
             i += 1
+            if plot_figures == True:
+                fig.add_subplot(row, column, i)
+
             print('Loading calibration images at ' + str(int(100 * i/(len(images)))) + ' %...')
             img = cv2.imread(fname)
-            plt.imshow(img)
-            plt.show()
-            #plt.show(wait = False)
+            if plot_figures == True:
+                plt.imshow(img)
+                        
             gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
         
             # Find the chessboard corners
@@ -40,10 +48,114 @@ class Camera():
                 self.objpoints.append(objp)
                 self.imgpoints.append(corners)
         
-                # Draw and display the corners
+                #Draw and display the corners
                 #img = cv2.drawChessboardCorners(img, (self.nx,self.ny), corners, ret)
                 #cv2.imshow('img',img)
-                #cv2.waitKey(200)
+                #cv2.waitKey(200) 
 
-        cv2.waitKey(1)
-        cv2.destroyAllWindows()
+        # Do camera calibration given object points and image points, by getting the matrix coefficients
+        self.ret, self.mtx, self.dist, self.rvecs, self.tvecs = cv2.calibrateCamera(self.objpoints, self.imgpoints, gray.shape[::-1], None, None)
+        
+        if plot_figures == True:
+            plt.show()
+
+        #cv2.waitKey(1)
+        #cv2.destroyAllWindows()
+
+    # Given an image return it's undistorted version
+    def undistort(self, img):
+        return cv2.undistort(img, self.mtx, self.dist, None, self.mtx)
+
+    def sobel_threshold(self, gray_img, orient='x', sobel_kernel=3, thresh=(0, 255)):
+        # Calculate directional gradient
+        if orient == 'x':
+            sobel = cv2.Sobel(gray_img, cv2.CV_64F, 1, 0, ksize=sobel_kernel)
+        else:
+            sobel = cv2.Sobel(gray_img, cv2.CV_64F, 0, 1, ksize=sobel_kernel)
+        abs_sobel = np.absolute(sobel) # Absolute x derivative to accentuate lines away from horizontal
+        scaled_sobel = np.uint8(255*abs_sobel/np.max(abs_sobel))
+        
+        # Apply threshold
+        binary = np.zeros_like(scaled_sobel)
+        binary[(scaled_sobel >= thresh[0]) & (scaled_sobel <= thresh[1])] = 1
+        return binary
+
+    def mag_threshold(self, gray_image, sobel_kernel=3, thresh=(0, 255)):
+        # Calculate gradient magnitude
+        # Apply threshold
+        sobelx = self.sobel_threshold(gray_image, 'x', sobel_kernel)
+        sobely = self.sobel_threshold(gray_image, 'y', sobel_kernel)
+        # Calculate the gradient magnitude
+        gradmag = np.sqrt(sobelx**2 + sobely**2)
+        # Rescale to 8 bit
+        scale_factor = np.max(gradmag)/255 
+        gradmag = (gradmag/scale_factor).astype(np.uint8) 
+        # Create a binary image of ones where threshold is met, zeros otherwise
+        binary_output = np.zeros_like(gradmag)
+        binary_output[(gradmag >= thresh[0]) & (gradmag <= thresh[1])] = 1
+
+        # Return the binary image
+        return binary_output
+
+    def dir_threshold(self, gray_image, sobel_kernel=3, thresh=(0, np.pi/2)):
+        # Calculate gradient magnitude
+        # Apply threshold
+        sobelx = self.sobel_threshold(gray_image, 'x', sobel_kernel)
+        sobely = self.sobel_threshold(gray_image, 'y', sobel_kernel)
+        # Take the absolute value of the gradient direction, 
+        # apply a threshold, and create a binary image result
+        absgraddir = np.arctan2(np.absolute(sobely), np.absolute(sobelx))
+        binary_output =  np.zeros_like(absgraddir)
+        binary_output[(absgraddir >= thresh[0]) & (absgraddir <= thresh[1])] = 1
+
+        # Return the binary image
+        return binary_output
+
+    # Define a function that takes an image and returns the combined color and gradient
+    # thresholds and the staked image
+    def binary_from_combined_thresholds(self, img, sobel_kernel = 3, thresh_color_s_channel = (0, 255), 
+            thresh_sobel_x = (20, 100), thresh_dir_gradient = (0, np.pi/2), thresh_magnitude = (0, 255)):
+    # Convert to HLS color space and separate the S channel
+        # Note: img is the undistorted image
+        hls = cv2.cvtColor(img, cv2.COLOR_RGB2HLS)
+        s_channel = hls[:,:,2]
+
+        # Grayscale image
+        # NOTE: we already saw that standard grayscaling lost color information for the lane lines
+        # Explore gradients in other colors spaces / color channels to see what might work better
+        gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+
+        # Sobel x & threshold x gradient
+        sxbinary = self.sobel_threshold(gray, 'x', sobel_kernel, thresh_sobel_x)
+
+        # Threshold color channel
+        s_binary = np.zeros_like(s_channel)
+        s_binary[(s_channel >= thresh_color_s_channel[0]) & (s_channel <= thresh_color_s_channel[1])] = 1
+
+        # Stack each channel to view their individual contributions in green and blue respectively
+        # This returns a stack of the two binary images, whose components you can see as different colors
+        color_binary = np.dstack(( np.zeros_like(sxbinary), sxbinary, s_binary)) * 255
+
+        # gradient Direction 
+        dir_binary = self.dir_threshold(gray, sobel_kernel, thresh_dir_gradient)
+        mag_binary = self.mag_threshold(gray, sobel_kernel, thresh_magnitude)
+
+        # Combine the two binary thresholds
+        combined_binary = np.zeros_like(sxbinary)
+        combined_binary[(s_binary == 1) | (sxbinary == 1) | ((dir_binary == 1) & (mag_binary == 1))] = 1
+
+        return combined_binary, color_binary
+        
+    # Define a function that takes an image, number of x and y points, 
+    # camera matrix and distortion coefficients and return the birdeye-view
+    def corners_unwarp(self, img, corners_source, corners_destination):
+        # Use the OpenCV undistort() function to remove distortion
+        # undist = cv2.undistort(img, self.mtx, self.dist, None, self.mtx)
+        undist = self.undistort(img)
+        img_size = (img.shape[1], img.shape[0])
+        self.perspectiveM = cv2.getPerspectiveTransform(corners_source, corners_destination)
+        # Warp the image using OpenCV warpPerspective()
+        warped = cv2.warpPerspective(undist, self.perspectiveM, img_size)
+        # Return the resulting image and matrix
+        return warped, self.perspectiveM
+        #return undist, img_size
--- source/main.py
+++ source/main.py
@@ -9,13 +9,156 @@ matplotlib.use('Qt4Agg')
 #'Qt5Agg', 'TkAgg', 'WX', 'gdk', 'GTK', 'GTK3Agg', 'svg', 'WebAgg', 'emf']
 #matplotlib.use('agg')
 import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+
 #%matplotlib qt
 #from IPython import get_ipython
 #get_ipython().run_line_magic('matplotlib', 'qt')
 from camera import Camera
 from line import Line
 
+def region_of_interest(img, vertices):
+    """
+    Applies an image mask.
+    
+    Only keeps the region of the image defined by the polygon
+    formed from `vertices`. The rest of the image is set to black.
+    `vertices` should be a numpy array of integer points.
+    """
+    #defining a blank mask to start with
+    mask = np.zeros_like(img)   
+    
+    #defining a 3 channel or 1 channel color to fill the mask with depending on the input image
+    if len(img.shape) > 2:
+        channel_count = img.shape[2]  # i.e. 3 or 4 depending on your image
+        ignore_mask_color = (255,) * channel_count
+    else:
+        ignore_mask_color = 255
+        
+    #filling pixels inside the polygon defined by "vertices" with the fill color    
+    cv2.fillPoly(mask, vertices, ignore_mask_color)
+    
+    #returning the image only where mask pixels are nonzero
+    masked_image = cv2.bitwise_and(img, mask)
+    return masked_image
+
+
+'''
+Compute the camera calibration matrix and distortion coefficients
+given a set of chessboard images.
+'''
+## Instantiate camera and and calibrate it
 cam = Camera()
 cam.calibrate()
-       
+fname = '../data/camera_cal/calibration1.jpg'
+example_img = mpimg.imread(fname)  
 
+''' 
+First rubric point: 
+Provide an example of a distortion-corrected image
+'''
+fig = plt.figure()
+#  load original image and display it
+undistorted_example_img = cam.undistort(example_img)
+fig_handle = fig.add_subplot(1, 2, 1)
+plt.imshow(example_img)
+fig_handle.set_title('Original image')
+plt.show(block = False)
+
+# undistort the image and display it to the same figure
+fig_handle =fig.add_subplot(1, 2, 2)
+plt.imshow(undistorted_example_img)
+fig_handle.set_title('Undistorted image')
+plt.show(block = False)
+
+
+'''
+Provide an example of a binary image result.
+'''
+# Use graphical interface to pick
+# Load test images
+images = glob.glob('../data/test_images/test*.jpg')
+
+sobel_kernel = 9
+thresh_color_s_channel = (90, 255) # from Gradients and Color Spaces: HLS Quiz
+thresh_sobel_x = (20, 100)
+thresh_dir_gradient = (0.7, 1.3)
+thresh_magnitude = (30, 100)
+'''
+230 max_y
+1240 max_y
+
+600 450
+840 450
+'''
+left_low = (100, 720)
+left_high = (600, 450)
+right_low = (1240, 720)
+right_high = (805, 450)
+vertices = np.array([[left_low, left_high, right_high, right_low]], dtype=np.int32)
+
+
+i = 0
+for fname in images:
+    fig = plt.figure(2)
+    img = mpimg.imread(fname) #in RGB
+    '''
+    Get combined binary image
+    '''
+    combined_binary, color_binary = cam.binary_from_combined_thresholds(
+        img, sobel_kernel, thresh_color_s_channel, thresh_sobel_x, 
+        thresh_dir_gradient, thresh_magnitude)
+
+    # Plotting thresholded images
+    fig_handle = fig.add_subplot(8, 3, 3 * i + 1)
+    plt.imshow(img)
+    #fig_handle.set_title('Original image')
+
+    fig_handle = fig.add_subplot(8, 3, 3 * i + 2)
+    plt.imshow(color_binary)
+    #fig_handle.set_title('Stacked thresholds')
+    
+    combined_binary = region_of_interest(combined_binary,vertices)
+    fig_handle = fig.add_subplot(8, 3, 3 * i + 3)
+    plt.imshow(combined_binary)
+    
+    # save to file
+    cv2.imwrite('../data/test_images/saved_from_algorithm/test' + str(i + 1) + '.jpg', combined_binary)
+    print('Saved in ../data/test_images/saved_from_algorithm/test' + str(i + 1) + '.jpg')
+ 
+    #fig_handle.set_title('Combined S channel and  gradient thresholds')
+
+    '''
+    # Unwarp and identify lane pixels, fit with polynomial and calculate curvature
+    '''
+    fig3 = plt.figure(3)
+    
+    fig_handle = fig3.add_subplot(8, 2, 2 * i + 1)
+    
+    offset1 = 330
+    offset2 = 450
+    
+    cp_img = np.copy(img)
+    cp_img = cv2.line(cp_img, (250, 720), (250 + offset1, 450), [255, 0, 0], 2)
+    cp_img = cv2.line(cp_img, (250 + offset1, 450), (1150 - offset2, 450), [255, 0, 0], 2)
+    cp_img = cv2.line(cp_img, (1150 - offset2, 450), (1150, 720), [255, 0, 0], 2)
+    
+    corners_source = np.float32([[255, 720], [255 + offset1, 450], [1150 - offset2, 450], [1150, 720]])
+    corners_destination = np.float32([[250, 720], [250, 0], [1150, 0], [1150, 720]])
+    # plot original and warped view
+    plt.imshow(cp_img)
+    fig_handle = fig3.add_subplot(8, 2, 2 * i + 2)
+    warped_img, M = cam.corners_unwarp(cp_img, corners_source, corners_destination)
+    plt.imshow(warped_img)
+    i += 1
+
+    '''
+    Binary warped image
+    '''
+    binary_warped = cam.corners_unwarp(combined_binary, corners_source, corners_destination)
+
+plt.show(block = False)
+
+
+
+input("Press Enter to continue...")
